d1be5f8a3f6ebeffa54c94309c491e55981c53a5
Leonardo Garcia <leonardo.garcia@wizeline.com>
2018-08-22 17:41:54 -0500

    Initial commit

---
+++ Whatcha!/Controllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Whatcha!
+//
+//  Created by Leonardo Garcia  on 8/22/18.
+//  Copyright © 2018 Leonardo Garcia. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func loadView() {
+        let customView = HomeView(frame: .zero)
+        view = customView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Whatcha!/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Whatcha!
+//
+//  Created by Leonardo Garcia  on 8/22/18.
+//  Copyright © 2018 Leonardo Garcia. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
